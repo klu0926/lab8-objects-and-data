7d8cda464a05c9abec04d245f7f64d3bece4cd0a
klu0926 <lukuoyu@gmail.com>
2025-04-05 20:07:26 -0400

    add part 3 using destructuring assignment

--- script.js
+++ script.js
@@ -10,14 +10,31 @@ const student = {
     console.log(`Hi I am ${this.name}, age ${this.age}, I am ${entrollText}.\nI am taking ${coursesText}`)
   }
 }
+console.log('1. Create student object')
 student.whoami()
 console.log('\n')
 
 // Part 2: Working with JSON
 // convert object to JSON
 const jsonString = JSON.stringify(student)
-console.log(`JSON STRING: ${jsonString}`)
 const object = JSON.parse(jsonString)
+console.log('2. Working with JSON')
+console.log(`JSON STRING: ${jsonString}`)
 console.log(`Covert JSON back to object:`, object)
+console.log('\n')
 
+// Part 3: Using Destructuring Assignment
+// extract name and coures
+const { name: studentName, courses: studentCourses } = student
+console.log('3. Using Destructuring Assignment')
+console.log('name:', studentName)
+console.log('courses:', studentCourses)
+
+// create array of scores
+const scores = [100, 95, 85, 99, 77, 65, 45]
+const [firstScore, secondScore] = scores
+console.log('scores:', scores)
+console.log('firstScore:', firstScore)
+console.log('secondScore:', secondScore)
+console.log('\n')
 
