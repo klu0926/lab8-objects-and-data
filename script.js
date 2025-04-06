4862f7ddfcea3fb73b40a4654356f7e8d714cbb9
klu0926 <lukuoyu@gmail.com>
2025-04-05 20:01:20 -0400

    finish part 2 working with JSON

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+// Part 1: Understanding and Creating Objects
 const student = {
   name: 'Peter',
   age: '12',
   enrolled: true,
   courses: ["math", "english", "science", "art of murder"],
   whoami() {
-    const entrollText = this.entrolled ? "currently enrolled" : "not enrolled"
+    const entrollText = this.enrolled ? "currently enrolled" : "not enrolled"
     const coursesText = this.courses.join(' , ')
     console.log(`Hi I am ${this.name}, age ${this.age}, I am ${entrollText}.\nI am taking ${coursesText}`)
   }
 }
 student.whoami()
+console.log('\n')
+
+// Part 2: Working with JSON
+// convert object to JSON
+const jsonString = JSON.stringify(student)
+console.log(`JSON STRING: ${jsonString}`)
+const object = JSON.parse(jsonString)
+console.log(`Covert JSON back to object:`, object)
+
+
